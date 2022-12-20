736688e32b7eb6194839ab2cd939a258e844618f
RealAnna <anna.reale@dynatrace.com>
2022-12-20 10:13:19 +0100

    rename
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- klt-cert-manager/controllers/keptnwebhookcontroller/keptnwebhookcertificate_controller.go
+++ klt-cert-manager/controllers/keptnwebhookcontroller/keptnwebhookcertificate_controller.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/keptn/lifecycle-toolkit/cert-manager/eventfilter"
+	"github.com/keptn/lifecycle-toolkit/klt-cert-manager/eventfilter"
 	"github.com/pkg/errors"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	appsv1 "k8s.io/api/apps/v1"
